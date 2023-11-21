0eaee674cc610a637bca385a199f3122a130873a
James Alcock <75587027+Jalco28@users.noreply.github.com>
2023-11-21 10:49:36 +0000

    Stupid semicolon

--- serverfiles/new.php
+++ serverfiles/new.php
@@ -7,7 +7,7 @@
     $score = $_POST["score"];
     $difficulty = $_POST["difficulty"];
     $date = $_POST["date"];
-    $actual_user  = $_POST["actual_user"]
+    $actual_user  = $_POST["actual_user"];
     shell_exec("python3 /var/www/html/doorsos/new.py $username $score $difficulty $date $actual_user")
 
 ?>
