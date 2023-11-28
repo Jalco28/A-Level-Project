aa5e80c50b718fd2c64196fc609b98082d0fe8fe
James Alcock <75587027+Jalco28@users.noreply.github.com>
2023-11-28 13:22:31 +0000

    Quotes around command arguments

--- serverfiles/new.php
+++ serverfiles/new.php
@@ -8,7 +8,7 @@
     $difficulty = $_POST["difficulty"];
     $date = $_POST["date"];
     $actual_user  = $_POST["actual_user"];
-    shell_exec("python3 /var/www/html/doorsos/new.py $username $score $difficulty $date $actual_user")
+    shell_exec("python3 /var/www/html/doorsos/new.py '$username' '$score' '$difficulty' '$date' '$actual_user'")
 
 ?>
 
